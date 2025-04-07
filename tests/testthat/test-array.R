22a4ad5504f8cd472678009704fd1f45030f16f0
Joakim Linde <joakim@lindelabs.org>
2025-04-07 20:42:20 +0300

    Remove test depenency on vctrs

--- tests/testthat/test-array.R
+++ tests/testthat/test-array.R
@@ -2,7 +2,6 @@ previous_edition = edition_get()
 local_edition(3)
 
 test_that("arrays can be read (INTEGER)", {
-  skip_if_not_installed("vctrs")
   con <- dbConnect(duckdb())
   on.exit(dbDisconnect(con, shutdown = TRUE))
 
@@ -12,19 +11,17 @@ test_that("arrays can be read (INTEGER)", {
   dbExecute(con, "INSERT INTO tbl VALUES (12, [3, 7, 11])")
   dbExecute(con, "INSERT INTO tbl VALUES (13, [4, 8, 12])")
 
-  df1 <- dbGetQuery(con, "FROM tbl")
+  df <- dbGetQuery(con, "FROM tbl")
 
-  df2 <- vctrs::data_frame(
-    a = c(10, 11, 12, 13),
-    b = matrix(1:12, nrow = 4, ncol = 3)
-  )
+  a = c(10, 11, 12, 13)
+  b = matrix(1:12, nrow = 4, ncol = 3)
 
-  expect_equal(df1, df2)
+  expect_equal(df$a, a)
+  expect_equal(df$b, b)
 })
 
 
 test_that("arrays can be read (DOUBLE)", {
-  skip_if_not_installed("vctrs")
   con <- dbConnect(duckdb())
   on.exit(dbDisconnect(con, shutdown = TRUE))
 
@@ -34,19 +31,17 @@ test_that("arrays can be read (DOUBLE)", {
   dbExecute(con, "INSERT INTO tbl VALUES (12, [3, 7, 11])")
   dbExecute(con, "INSERT INTO tbl VALUES (13, [4, 8, 12])")
 
-  df1 <- dbGetQuery(con, "FROM tbl")
+  df <- dbGetQuery(con, "FROM tbl")
 
-  df2 <- vctrs::data_frame(
-    a = c(10, 11, 12, 13),
-    b = matrix(as.double(1:12), nrow = 4, ncol = 3)
-  )
+  a = c(10, 11, 12, 13)
+  b = matrix(as.double(1:12), nrow = 4, ncol = 3)
 
-  expect_equal(df1, df2)
+  expect_equal(df$a, a)
+  expect_equal(df$b, b)
 })
 
 
 test_that("arrays can be read (BOOLEAN)", {
-  skip_if_not_installed("vctrs")
   con <- dbConnect(duckdb())
   on.exit(dbDisconnect(con, shutdown = TRUE))
 
@@ -56,18 +51,17 @@ test_that("arrays can be read (BOOLEAN)", {
   dbExecute(con, "INSERT INTO tbl VALUES (12, [true, true, false])")
   dbExecute(con, "INSERT INTO tbl VALUES (13, [false, false, false])")
 
-  df1 <- dbGetQuery(con, "FROM tbl")
+  df <- dbGetQuery(con, "FROM tbl")
 
-  df2 <- vctrs::data_frame(
-    a = c(10, 11, 12, 13),
-    b = matrix( c(T, F, T, F, F, T, T, F, T, T, F, F) , nrow = 4, ncol = 3 )
-  )
+  a = c(10, 11, 12, 13)
+  b = matrix( c(T, F, T, F, F, T, T, F, T, T, F, F) , nrow = 4, ncol = 3 )
 
-  expect_equal(df1, df2)
+  expect_equal(df$a, a)
+  expect_equal(df$b, b)
 })
 
 
-test_that("array error with more than one dimention", {
+test_that("array errors with more than one dimention", {
   con <- dbConnect(duckdb())
   on.exit(dbDisconnect(con, shutdown = TRUE))
 
